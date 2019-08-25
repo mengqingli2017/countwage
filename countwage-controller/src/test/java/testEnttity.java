db7d81045c687b82ee7d9e893ef3df135d13b006
蒙庆利 <1078149674@qq.com>
2019-08-26 01:00:12 +0800

    2019-8-25更新ssm配置

---
+++ countwage-controller/src/test/java/testEnttity.java
@@ -0,0 +1,13 @@
+import com.qingli.entity.User;
+
+/**
+ * @Classname testEnttity
+ * @author: 蒙庆利
+ * @Date 2019/8/26 0:22
+ * @Description:
+ */
+public class testEnttity {
+    public void testEntity(){
+        new User()
+    }
+}
---
+++ countwage-entity/src/main/java/com/qingli/entity/User.java
@@ -0,0 +1,19 @@
+package com.qingli.entity;
+
+/**
+ * @Classname User
+ * @author: 蒙庆利
+ * @Date 2019/8/26 0:23
+ * @Description:
+ */
+public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
